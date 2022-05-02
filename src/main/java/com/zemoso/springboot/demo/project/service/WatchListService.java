38bce0feca08468f33806bdc35ee65b61e820f2c
akalintha007 <akalinthaofficial@gmail.com>
2022-05-02 17:08:14 +0530

    first commit

---
+++ src/main/java/com/zemoso/springboot/demo/project/service/WatchListService.java
@@ -0,0 +1,20 @@
+package com.zemoso.springboot.demo.project.service;
+
+import com.zemoso.springboot.demo.project.entity.Anime;
+import com.zemoso.springboot.demo.project.entity.WatchList;
+
+import java.util.List;
+
+public interface WatchListService {
+
+    public List<Anime> getWatchListByUser(String userName);
+
+    public void removeFromWatchList(String userName, int theId);
+
+    public void addToWatchList(String userName, int theId);
+
+
+    public void deleteFromWatchList( int theId);
+
+    public int checkBeforeAddToWatchList(String userName, int theId);
+}
