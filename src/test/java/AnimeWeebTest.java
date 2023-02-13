37d377875d32219dabe20a55c78a7a1f606aa814
Akalintha007 <93814531+Akalintha007@users.noreply.github.com>
2023-02-13 14:45:48 +0530

    added test cases

--- src/test/java/AnimeWeebTest.java
+++ src/test/java/AnimeWeebTest.java
@@ -1,5 +1,3 @@
-package com.zemoso.springboot.demo.project;
-
 import com.zemoso.springboot.demo.project.constants.AppConstants;
 import com.zemoso.springboot.demo.project.controller.AnimeController;
 import com.zemoso.springboot.demo.project.controller.ErrorAndExceptionController;
